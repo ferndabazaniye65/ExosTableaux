ffa22fc5e8e59a663341153f13e2077d6ecd79a2
Ferdinandsirius <ferndabazaniye65@gmail.com>
2023-08-04 12:59:05 +0200

    Ajout de l'exercice 1 et 2

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('ca fonctionne');
+let myAlphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "$",
+    "*",
+    "/",
+    "-",
+    "+",
+];
+  
+function myAlphabetLength() {
+    console.log(myAlphabet.length);
+}
+myAlphabetLength();
+
+// exo 2
+
+const planets = ["Vénus", "Uranus", "Terre", "Mercure", "Jupiter"];
+
+planets.forEach((planet, index) => {
+    console.log(planet, index);
+});
